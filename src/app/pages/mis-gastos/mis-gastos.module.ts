f99ffb8712fa77bfce57138c21af37148c06f2df
Luis Arlex Buenrostro Adame <a16300046@live.ceti.mx>
2020-01-29 17:58:06 -0600

    Mis gastos y modal
    Hice el diseño de la seccion mis gastos y actualice la seccion del modal de registro

---
+++ src/app/pages/mis-gastos/mis-gastos.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { MisGastosPageRoutingModule } from './mis-gastos-routing.module';
+
+import { MisGastosPage } from './mis-gastos.page';
+import { ComponentsModule } from '../../components/components.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    MisGastosPageRoutingModule,
+    ComponentsModule
+  ],
+  declarations: [MisGastosPage]
+})
+export class MisGastosPageModule {}
