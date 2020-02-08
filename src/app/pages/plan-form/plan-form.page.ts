5542ba267decca40737c1b51120ca6b00d66cd28
Arlex <labarlex17@hotmail.com>
2020-02-08 13:18:52 -0600

    formulario_plan2

--- src/app/pages/plan-form/plan-form.page.ts
+++ src/app/pages/plan-form/plan-form.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Plan } from '../../interfaces/interfaces';
 
 @Component({
   selector: 'app-plan-form',
@@ -6,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./plan-form.page.scss'],
 })
 export class PlanFormPage implements OnInit {
+
+planNuevo: Plan = {
+  nombre: '',
+  cantidadTotal: null,
+  tiempoTotal: null,
+  cantidadAcumulada: null,
+  tiempoRestante: null,
+  descripcion: '',
+  aportacionMensual: null,
+};
 
   constructor() { }
 
